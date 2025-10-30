8e08201ef516bdf90b20f131b846edbe41397119
Твоё Имя <samir.sib2016@gmail.com>
2025-10-30 19:56:06 +0300

    fix

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: './',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        index: 'index.html',
+        currency: 'currency.html',
+        anime: 'anime.html',
+        waifu: 'waifu.html',
+      },
+    },
+  },
+})
