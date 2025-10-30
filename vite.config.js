8f7000b562d2b5a8493ed7b9086e76fbc6b796b9
Твоё Имя <samir.sib2016@gmail.com>
2025-10-30 20:00:28 +0300

    fixxxx

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     outDir: 'dist',
     rollupOptions: {
       input: {
-        index: 'index.html',
+        main: 'index.html',
         currency: 'currency.html',
         anime: 'anime.html',
         waifu: 'waifu.html',
